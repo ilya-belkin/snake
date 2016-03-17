a82e0d94c94e80a8f2bf6aadadbc3d67db784141
ilya-belkin <ilya-belkin@list.ru>
2016-03-17 14:54:55 +0300

    Создал список

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,6 +15,16 @@ namespace Snake
             Point p2 = new Point(6, 7, '#');
             p2.Draw();
 
+            List<Point> pList = new List<Point>();
+
+            Point p3 = new Point(2, 4, '@');
+            Point p4 = new Point(5, 2, '$');
+
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
             Console.ReadLine();
         }
     }
